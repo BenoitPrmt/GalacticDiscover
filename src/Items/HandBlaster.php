40c2084399d30b9a38c1f3074e1b119b46924944
Raxuis <raclot.raphael@gmail.com>
2024-09-26 16:33:45 +0200

    fix(spelling mistakes): :bug: fix english spelling mistakes

--- src/Items/HandBlaster.php
+++ src/Items/HandBlaster.php
@@ -1,23 +1,29 @@
 <?php
 
 namespace GalacticDiscover\Items;
+
 use Jugid\Staurie\Game\Item_Equippable;
 
-class HandBlaster extends Item_Equippable {
-    public function name() : string {
-        return "Hand-Blaster";
-    }
-    public function description(): string {
-        return "Careful it's hurt";
-    }
-    public function body_part(): string {
-        return "hand";
-    }
-    public function statistics(): array {
-        return [
-            'damages'=> 2,
-            'defense'=> 0,
-            'range' => 2
-        ];
-    }
+class HandBlaster extends Item_Equippable
+{
+  public function name(): string
+  {
+    return "Hand-Blaster";
+  }
+  public function description(): string
+  {
+    return "Careful it hurts.";
+  }
+  public function body_part(): string
+  {
+    return "hand";
+  }
+  public function statistics(): array
+  {
+    return [
+      'damages' => 2,
+      'defense' => 0,
+      'range' => 2
+    ];
+  }
 }
--- src/Items/ShoulderBlaster.php
+++ src/Items/ShoulderBlaster.php
@@ -1,23 +1,29 @@
 <?php
 
 namespace GalacticDiscover\Items;
+
 use Jugid\Staurie\Game\Item_Equippable;
 
-class ShoulderBlaster extends Item_Equippable {
-    public function name() : string {
-        return "Shoulder-Blaster";
-    }
-    public function description(): string {
-        return "Be Careful it's dangerous";
-    }
-    public function body_part(): string {
-        return "hand";
-    }
-    public function statistics(): array {
-        return [
-            'damages'=> 3,
-            'defense'=> 0,
-            'range' => 2
-        ];
-    }
+class ShoulderBlaster extends Item_Equippable
+{
+  public function name(): string
+  {
+    return "Shoulder-Blaster";
+  }
+  public function description(): string
+  {
+    return "Be careful it's dangerous";
+  }
+  public function body_part(): string
+  {
+    return "hand";
+  }
+  public function statistics(): array
+  {
+    return [
+      'damages' => 3,
+      'defense' => 0,
+      'range' => 2
+    ];
+  }
 }
