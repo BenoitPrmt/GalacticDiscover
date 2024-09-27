d00a516b3f61023f18c140e59779b275a10a361c
BenoitPrmt <benoitparmentier45@gmail.com>
2024-09-27 12:29:40 +0200

    feat(fight): added fight feature (fight <name>)

--- src/Components/Combat/Combat.php
+++ src/Components/Combat/Combat.php
@@ -5,7 +5,9 @@ namespace GalacticDiscover\Components\Combat;
 use GalacticDiscover\Components\Combat\Functions\FightFunction;
 use Jugid\Staurie\Component\AbstractComponent;
 use Jugid\Staurie\Component\Character\MainCharacter;
+use Jugid\Staurie\Component\Character\Statistics;
 use Jugid\Staurie\Component\PrettyPrinter\PrettyPrinter;
+use Jugid\Staurie\Game\Monster;
 
 class Combat extends AbstractComponent {
     public function getEventName(): array {
@@ -22,12 +24,95 @@ class Combat extends AbstractComponent {
     private function begin(string $monsterName): void
     {
         $pp = $this->container->getPrettyPrinter();
-        $pp->write('Début du combat contre ');
+        $character = $this->container->getCharacter();
+
+        if (!$character->statistics->has('damages')) {
+            $pp->writeLn("You can't fight with no equipment !", 'white', 'red');
+            return;
+        }
+
+        $pp->write($character->name, 'green');
+        $pp->write(' VS ');
         $pp->writeLn($monsterName, 'red');
 
-        $character = $this->container->getCharacter();
-        $pp->writeLn($character->config['hp']);
 
+        $map = $this->container->getMap();
+        $enemy = null;
+        foreach($map->getCurrentBlueprint()->getMonsters() as $monster) {
+            if(!$monster instanceof Monster) {
+                continue;
+            }
+            if ($monster->name() === $monsterName) {
+                $enemy = [
+                    'name' => $monster->name(),
+                    'description' => $monster->description(),
+                    'level' => $monster->level(),
+                    'hp' => $monster->health_points(),
+                    'skills' => $monster->skills(),
+                    'defense' => $monster->defense()
+                ];
+                break;
+            }
+        }
+
+        $userHpTotal = $character->statistics->value('hp');
+        $userHp = $userHpTotal;
+        $enemyHpTotal = $enemy['hp'];
+        $enemyHp = $enemyHpTotal;
+
+        $round = 1;
+        while ($userHp > 0 or $enemyHp > 0) {
+            $pp->writeSeparator(centered: true);
+            $pp->writeLn("ROUND N°" . $round, centered: true);
+
+            $roundAttackers = [
+                [
+                    'attacker' => [
+                        'name' => $character->name,
+                        'damage' => $character->statistics->value('damages') * 20
+                    ],
+                    'victim' => [
+                        'name' => $monsterName,
+                        'defense' => $enemy['defense'],
+                        'hp' => &$enemyHp,
+                        'totalHp' => $enemyHpTotal
+                    ]
+                ],
+                [
+                    'attacker' => [
+                        'name' => $monsterName,
+                        'damage' => $enemy['skills']['attack'] * 20
+                    ],
+                    'victim' => [
+                        'name' => $character->name,
+                        'defense' => $character->statistics->value('defense'),
+                        'hp' => &$userHp,
+                        'totalHp' => $userHpTotal
+                    ]
+                ]
+            ];
+
+            foreach ($roundAttackers as $roundTurn) {
+                $pp->writeSeparator(centered: true);
+                $pp->writeLn($roundTurn['attacker']['name'] . " is attacking...", 'white', 'red');
+
+                $attackerDamage = $roundTurn['attacker']['damage'];
+                $victimDefense = $roundTurn['victim']['defense'];
+                $damageOnVictim = ($attackerDamage - $victimDefense) > 0 ? $attackerDamage - $victimDefense : 0;
+
+                if ($damageOnVictim === 0) {
+                    $pp->writeLn($roundTurn['victim']['name'] . " has protected himself and took 0 HP");
+                } else {
+                    $roundTurn['victim']['hp'] -= $damageOnVictim;
+                    $roundTurn['victim']['hp'] = max($roundTurn['victim']['hp'], 0);
+
+                    $pp->writeLn($roundTurn['attacker']['name'] . "hit and remove " . $damageOnVictim . "HP to " . $roundTurn['victim']['name']);
+                }
+                $pp->writeProgressbar($roundTurn['victim']['hp'], max: $roundTurn['victim']['totalHp']);
+            }
+            // TODO: check end condition
+            $round++;
+        }
     }
     public function name(): string {
         return "combat";
