f4752ee629742b916419af09c2f4b4d949b38821
Raxuis <raclot.raphael@gmail.com>
2024-09-27 09:55:02 +0200

    refactor(character): :busts_in_silhouette: edited the body parts of the player

--- main.php
+++ main.php
@@ -11,42 +11,50 @@ use Jugid\Staurie\Component\PrettyPrinter\PrettyPrinter;
 use Jugid\Staurie\Staurie;
 use GalacticDiscover\Components\Combat\Combat;
 
-require_once __DIR__.'/vendor/autoload.php'; //A REMPLACER
+require_once __DIR__ . '/vendor/autoload.php'; //A REMPLACER
 
-$staurie= new Staurie('Galactic Discover');
+$staurie = new Staurie('Galactic Discover');
 $staurie->register(
-    [
-        Console::class,
-        PrettyPrinter::class,
-        MainCharacter::class,
-        Inventory::class,
-        Level::class,
-        Combat::class
-    ]
+  [
+    Console::class,
+    PrettyPrinter::class,
+    MainCharacter::class,
+    Inventory::class,
+    Level::class,
+    Combat::class
+  ]
 );
 
-$container= $staurie->getContainer();
-$menu= $container->registerComponent(Menu::class);
+$container = $staurie->getContainer();
+$menu = $container->registerComponent(Menu::class);
 $menu->configuration([
-    'text'=> 'Welcome to this awesome test adventure',
-    'labels'=> [
-        'new_game' => 'Enter the world',
-        'quit'=> 'Exit game',
-    ]
+  'text' => 'Welcome to this awesome test adventure',
+  'labels' => [
+    'new_game' => 'Enter the world',
+    'quit' => 'Exit game',
+  ]
 ]);
 
 $map = $container->registerComponent(Map::class);
 $map->configuration([
-    'directory' => __DIR__ . '/src/Maps',
-    'namespace' => 'GalacticDiscover\Maps'
+  'directory' => __DIR__ . '/src/Maps',
+  'namespace' => 'GalacticDiscover\Maps'
 ]);
 
 $charStats = new Statistics();
-$charStats->addDefault('hp', 100);
+$charStats->addDefault('hp', default_value: 100);
 
 $character = $container->registerComponent(MainCharacter::class);
 $character->configuration([
-    'statistics' => $charStats
+  'statistics' => $charStats,
+  'equipment' => [
+    'head' => null,
+    'hand' => null,
+    'shield' => null,
+    'shoulders' => null,
+    'back' => null,
+    'outfit' => null,
+  ]
 ]);
 
 $staurie->run();
